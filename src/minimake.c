69542d031791787bae114b5e86c4769b41bc7da1
William Lin <william.lin@epita.fr>
2020-04-22 01:32:23 +0200

    mnk: Add Makefile selection

--- src/minimake.c
+++ src/minimake.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #include "lexer.h"
 #include "parser.h"
@@ -6,15 +9,34 @@
 #include "opt_parser.h"
 
 
+
 int main(int argc, char* argv[])
 {
     struct options opts = opt_parse(argc, argv);
-    printf("%s\n", opts.filename);
-    for (; opts.nonopts < argc; opts.nonopts++)
+    
+    char filename[256];
+    memset(filename, 0, 256);
+    if (opts.filename)
+        strncpy(filename, opts.filename, 255);
+    if(!strlen(filename) && !access("makefile", F_OK))
+        strcpy(filename, "makefile");
+    if(!strlen(filename) && !access("Makefile", F_OK))
+        strcpy(filename, "Makefile");
+    if(!strlen(filename))
     {
-        printf("%s\n", argv[opts.nonopts]);
+        fprintf(stderr,
+                "%s: *** No targets specified and no makefile found.  Stop.\n",
+                argv[0]);
+        exit(2);
     }
 
+    printf("%s", filename);
+
+
+
+    // For later
+    //for (; opts.nonopts < argc; opts.nonopts++)
+        //exec_target(argv[opts.nonopts]);
 
     return 0;
 }
