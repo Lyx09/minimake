c2d541f4fbcd5bed1129f03818448d5b93febd4d
William Lin <william.lin@epita.fr>
2020-04-22 00:48:36 +0200

    opt: Add option parser to minimake

--- src/minimake.c
+++ src/minimake.c
@@ -3,14 +3,12 @@
 #include "lexer.h"
 #include "parser.h"
 #include "exec.h"
+#include "opt_parser.h"
+
 
 int main(int argc, char* argv[])
 {
-    // Useless for now
-    (void) argc;
-    (void) argv;
-
-    printf("Hello world !");
+    opt_parse(argc, argv);
 
     return 0;
 }
