b3e73b5c7abade0bac4f6ffb50e85489dd07900d
William Lin <william.lin@epita.fr>
2020-04-24 01:36:38 +0200

    make: Add free functions

--- src/minimake.c
+++ src/minimake.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-#include "lexer.h"
-#include "parser.h"
-#include "exec.h"
-#include "opt_parser.h"
 #include "common.h"
+#include "exec.h"
+#include "lexer.h"
+#include "opt_parser.h"
+#include "parser.h"
+#include "pretty_print.h"
 #include "vector.h"
 
-void pretty_print(struct vector* targets, struct vector* vars)
+void free_vars(struct vector *vars)
 {
-    (void) targets;
-    (void) vars;
+    for (size_t i = 0; i < vars->size; i++)
+    {
+        struct var *v = vector_get(vars, i);
+        free(v->line);
+        free(v);
+    }
+    vector_destroy(vars);
     return;
 }
 
-int main(int argc, char* argv[])
+void free_targets(struct vector *targets)
+{
+    for (size_t i = 0; i < targets->size; i++)
+    {
+        struct target *t = vector_get(targets, i);
+        struct vector *commands = t->commands;
+        for (size_t j = 0; j < commands->size; j++)
+            free(vector_get(commands, j));
+
+        vector_destroy(t->dependencies);
+        vector_destroy(commands);
+        free(t->line);
+        free(t);
+    }
+    vector_destroy(targets);
+    return;
+}
+
+int main(int argc, char *argv[])
 {
     struct options opts = opt_parse(argc, argv);
     
@@ -38,8 +62,8 @@ int main(int argc, char* argv[])
     }
 
 
-    struct vector *targets = vector_init(10);
-    struct vector *vars = vector_init(10);
+    struct vector *targets = vector_init(10);   // vector of struct target*
+    struct vector *vars = vector_init(10);      // vector of struct var*
 
     parse(filename, targets, vars);
     
@@ -55,8 +79,8 @@ int main(int argc, char* argv[])
         //exec_target(argv[opts.nonopts]);
 
     // some free time
-    vector_destroy(targets);
-    vector_destroy(vars);
+    free_vars(vars);
+    free_targets(targets);
     
     return RC_SUCCESS;
 }
